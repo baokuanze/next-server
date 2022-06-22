f4b6bc2340bf0b941752be172dc881243f61a65c
baokuanze <baokuanze@yidian-inc.com>
2022-06-22 10:23:38 +0800

    update file2

--- api/details.js
+++ api/details.js
@@ -1,16 +1,8 @@
 export default function handler(req, res) {
   res.status(200).json({
     result: {
-      data: [{
-        id: '123',
-        name: '123 test'
-      }, {
-        id: '456',
-        name: '456 tests'
-      }, {
-        id: '789',
-        name: '789 tests'
-      }]
+      data: '这是测试details'
     }
   })
 }
+
--- api/list.js
+++ api/list.js
@@ -1,9 +1,19 @@
 
 
+
 export default function handler(req, res) {
   res.status(200).json({
     result: {
-      data: '这是测试details'
+      data: [{
+        id: '123',
+        name: '123 test'
+      }, {
+        id: '456',
+        name: '456 tests'
+      }, {
+        id: '789',
+        name: '789 tests'
+      }]
     }
   })
 }
