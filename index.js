7a021808970cdeff930b4efd35474ec691ea2fe7
baokuanze <baokuanze@yidian-inc.com>
2022-07-26 17:22:54 +0800

    update file12

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
-// const Koa = require('koa');
-// const app = new Koa();
+const Koa = require('koa');
+const _ = require('./router/index.js')
+const app = new Koa();
 
-// app.use(async ctx => {
-//     ctx.body = 'Hello Vercel, Hi Koa2';
-// });
+app.use(_.routes())
 
-// app.listen(3008, () => {
-//     console.log('3008项目启动')
-// });
-
+app.use(async ctx => {
+    ctx.body = 'Hello Vercel, Hi Koa2';
+});
 
 
-
-const express = require('express')
-const Routers = require('./router/index')
-
-//  // "rewrites": [{ "source": "/api/(.*)", "destination": "/api" }]
-const port = parseInt(process.env.PORT, 10) || 3002
-const app = express()
+app.listen(3008, () => {
+    console.log('3008项目启动')
+});
 
 
-app.use("/api", Routers)
 
 
-app.listen(port, () => {
-  console.log(`> Ready on http://localhost:${port}`)
-})
+// const express = require('express')
+// const Routers = require('./router/index')
+
+// //  // "rewrites": [{ "source": "/api/(.*)", "destination": "/api" }]
+// const port = parseInt(process.env.PORT, 10) || 3002
+// const app = express()
+
+
+// app.use("/api", Routers)
+
+
+// app.listen(port, () => {
+//   console.log(`> Ready on http://localhost:${port}`)
+// })
--- router/index.js
+++ router/index.js
@@ -1,15 +1,48 @@
 
 
-const express = require('express')
-const router = express.Router()
+// const express = require('express')
+// const router = express.Router()
 
-router.use(function timeLog (req, res, next) {
-  console.log('Time: ', Date.now(), req.query)
-  next()
-})
+// router.use(function timeLog (req, res, next) {
+//   console.log('Time: ', Date.now(), req.query)
+//   next()
+// })
 
-router.get('/list', function (req, res) {
-  res.status(200).json({
+// router.get('/list', function (req, res) {
+//   res.status(200).json({
+//     result: {
+//       data: [{
+//         id: '123',
+//         name: '12345 test'
+//       }, {
+//         id: '456',
+//         name: '456 tests'
+//       }, {
+//         id: '789',
+//         name: '789 tests'
+//       }, {
+//         id: '789',
+//         name: '789 tests'
+//       }]
+//     }
+//   })
+// })
+
+// router.get('/details', function (req, res) {
+//   res.status(200).json({
+//     result: {
+//       data: '这是测试details2'
+//     }
+//   })
+// })
+
+
+
+
+const router = require('koa-router')();
+
+router.get('/list', function (ctx) {
+  ctx.body = {
     result: {
       data: [{
         id: '123',
@@ -21,19 +54,21 @@ router.get('/list', function (req, res) {
         id: '789',
         name: '789 tests'
       }, {
-        id: '7891',
-        name: '7891 tests'
+        id: '789',
+        name: '789 tests'
       }]
     }
-  })
+  }
 })
 
-router.get('/details', function (req, res) {
-  res.status(200).json({
+router.get('/details', function (ctx) {
+  ctx.body = {
     result: {
       data: '这是测试details2'
     }
-  })
+  }
+
 })
+
 
 module.exports = router
