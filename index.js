9f249c05f9398a80e267ff20df68e0f1626f0a95
baokuanze <baokuanze@yidian-inc.com>
2022-07-26 17:03:10 +0800

    update file8

--- index.js
+++ index.js
@@ -1,33 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 3003
+const Koa = require('koa');
+const app = new Koa();
 
-app.get('/api/list', (req, res) => {
-  res.status(200).json({
-    result: {
-      data: [{
-        id: '123',
-        name: '123 test'
-      }, {
-        id: '456',
-        name: '456 tests'
-      },  {
-        id: '789',
-        name: '789 tests'
-      }]
-    }
-  })
-})
+app.use(async ctx => {
+    ctx.body = 'Hello Vercel, Hi Koa2';
+});
 
 
-app.get('/api/detail', (req, res) => {
-  res.status(200).json({
-    result: {
-      data: '这是测试details'
-    }
-  })
-})
-
-app.listen(3003, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.listen(3008, () => {
+    console.log('3008项目启动')
+});
