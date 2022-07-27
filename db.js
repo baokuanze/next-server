a38864c75aaf561afd8c0208684341762911e534
baokuanze <baokuanze@yidian-inc.com>
2022-07-27 12:28:42 +0800

    update file1

--- db.js
+++ db.js
@@ -1,25 +1,16 @@
-/**
- * @description mongoose 连接数据库
- * @author liyunfu
- */
  const mongoose = require('mongoose')
-
- const DB_URL = 'mongodb://root:example@47.99.147.11.27017/jingdong?authSource=admin'
- 
+ const DB_URL = 'mongodb+srv://bao:asdbkz1234.as@cluster0.yxcw9.mongodb.net?retryWrites=true&w=majority'
  // 开始连接
  mongoose.connect(DB_URL, {
    useNewUrlParser: true,
    useUnifiedTopology: true
  })
- 
  // 连接对象
  const db = mongoose.connection
- 
  db.on('error', err => {
    console.error('mongoose connect error', err)
  })
  db.once('open', () => {
    console.log('mongoose 连接成功')
  })
- 
  module.exports = mongoose
--- index.js
+++ index.js
@@ -1,32 +1,15 @@
 const Koa = require('koa');
 const _ = require('./router/index.js')
+const mongo = require('./db')
 const app = new Koa();
 
 app.use(_.routes())
 
 app.use(async ctx => {
   console.log(ctx, ctx.request.url)
-    ctx.body = 'Hello Vercel, Hi Koa2';
+    ctx.body = 'Hello Word @泽';
 });
 
 app.listen(3008, () => {
     console.log('3008项目启动')
 });
-
-
-
-
-// const express = require('express')
-// const Routers = require('./router/index')
-
-// //  // "rewrites": [{ "source": "/api/(.*)", "destination": "/api" }]
-// const port = parseInt(process.env.PORT, 10) || 3002
-// const app = express()
-
-
-// app.use("/api", Routers)
-
-
-// app.listen(port, () => {
-//   console.log(`> Ready on http://localhost:${port}`)
-// })
